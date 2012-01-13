52afc0186b932839ae4f1ab9788d3ed687c3c759
Eric Rochester <err8n@virginia.edu>
2012-01-13 09:52:27 -0500

    Load the database library.

--- alumna.php
+++ alumna.php
@@ -6,7 +6,9 @@
 
 define('ALUMNA_DIR', dirname(__FILE__));
 define('TEMPLATE_DIR', ALUMNA_DIR . '/templates');
+define('DB_CONFIG_FILE', ALUMNA_DIR . '/config/database.ini');
 
 require_once ALUMNA_DIR . '/libs/Mustache.php';
 require_once ALUMNA_DIR . '/libs/MustacheLoader.php';
 require_once ALUMNA_DIR . '/libs/limonade.php';
+require_once ALUMNA_DIR . '/libs/db_manager.php';
