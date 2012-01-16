4722cf787c5d7e821134b834ebcc90d6ecd77f4d
Wayne Graham <wayne.graham@virginia.edu>
2012-01-16 09:24:56 -0500

    Added symlink for database config; changed repo to git

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,10 @@ require 'capistrano/ext/multistage'
 
 
 set :application, "alumna"
-set :repository,  "https://subversion.lib.virginia.edu/repos/alumna/trunk"
+#set :repository,  "https://subversion.lib.virginia.edu/repos/alumna/trunk"
+set :repository, 'git://github.com/waynegraham/alumna.git'
+set :branch, "develop"
+
 
 set :deploy_to, "/usr/local/projects/#{application}/"
 set :deploy_via, :remote_cache
@@ -18,18 +21,23 @@ ssh_options[:host_key] = 'ssh-dss'
 ssh_options[:paranoid] = false
 
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+#set :scm, :subversion
+set :scm, :git
 
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+after 'deploy', 'deploy:db_symlink', 'deploy:cleanup'
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart do ; end
+
+  desc 'Symlink the database.ini file in the correct place'
+  task :db_symlink, :except => {:no_release => true } do
+    run "cd #{current_path}/config && ln -s #{shared_path}/database.ini"
+  end
+
+end
+
+
